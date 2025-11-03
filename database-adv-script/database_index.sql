a6ff1d8152967d0e1cb43ce8d883796f45d5783e
olaniyi Emmanuel <emmanuel4sucess@outlook.com>
2025-11-03 21:24:14 +0100

    database optimization

---
+++ database-adv-script/database_index.sql
@@ -0,0 +1,45 @@
+-- Command to create index on booking and payment table 
+-- On booking table
+CREATE INDEX idx_booking_user_id ON booking(user_id);
+CREATE INDEX idx_booking_property_id ON booking(property_id);
+CREATE INDEX idx_booking_created_at ON booking(created_at);
+
+-- On payment table
+CREATE INDEX idx_payment_booking_id ON payment(booking_id);
+
+EXPLAIN
+SELECT
+  b.booking_id,
+  b.status AS booking_status,
+  b.created_at AS booking_date,
+
+  -- User details
+  u.user_id,
+  u.first_name,
+  u.last_name,
+  u.email,
+  u.phone_number,
+
+  -- Property details
+  p.property_id,
+  p.name AS property_name,
+  p.location,
+  p.description,
+
+  -- Payment details
+  pay.payment_id,
+  pay.amount,
+  pay.payment_method,
+  pay.payment_status,
+  pay.payment_date
+
+FROM booking b
+INNER JOIN users u 
+  ON b.user_id = u.user_id
+INNER JOIN property p 
+  ON b.property_id = p.property_id
+LEFT JOIN payment pay 
+  ON b.booking_id = pay.booking_id
+ORDER BY b.created_at DESC;
+
+SELECT * FROM payment; 
